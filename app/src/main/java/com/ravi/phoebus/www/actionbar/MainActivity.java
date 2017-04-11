cfb64dad159281ed130d6365073d582f4583f977
Ravi Gupta <ravik.rkg@gmail.com>
2017-04-11 19:35:36 +0530

    Add files

--- app/src/main/java/com/ravi/phoebus/www/actionbar/MainActivity.java
+++ app/src/main/java/com/ravi/phoebus/www/actionbar/MainActivity.java
@@ -1,9 +1,10 @@
 package com.ravi.phoebus.www.actionbar;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBar;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -27,6 +28,44 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_activity_bar,menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView =
+                (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        // Configure the search info and add any event listeners...
+
+        MenuItemCompat.OnActionExpandListener onActionExpandListener = new MenuItemCompat.OnActionExpandListener() {
+            /**
+             * Called when a menu item with
+             * is expanded.
+             *
+             * @param item Item that was expanded
+             * @return true if the item should expand, false if expansion should be suppressed.
+             */
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                Toast.makeText(MainActivity.this,"Expanded",Toast.LENGTH_SHORT).show();
+                return true;
+            }
+
+            /**
+             * Called when a menu item with
+             * is collapsed.
+             *
+             * @param item Item that was collapsed
+             * @return true if the item should collapse, false if collapsing should be suppressed.
+             */
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                Toast.makeText(MainActivity.this,"collapsed",Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        };
+        MenuItem actionMenuItem = menu.findItem(R.id.action_search);
+
+        // Assign the listener to that action item
+        MenuItemCompat.setOnActionExpandListener(actionMenuItem, onActionExpandListener);
         return true;
     }
 
